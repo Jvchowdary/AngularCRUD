7907a0fe4d6228943c61b6994a05d8fef94139c9
Jvchowdary <jaladi.choudary@gmail.com>
2024-07-06 14:42:56 +0530

    initialcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { AddEditDepartmentComponent } from './department/add-edit-department/add-edit-department.component';
+import { ShowDepartmentComponent } from './department/show-department/show-department.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AddEditEmployeeComponent } from './employee/add-edit-employee/add-edit-employee.component';
+import { ShowEmployeeComponent } from './employee/show-employee/show-employee.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    AddEditDepartmentComponent,
+    ShowDepartmentComponent,
+    EmployeeComponent,
+    AddEditEmployeeComponent,
+    ShowEmployeeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/department/add-edit-department/add-edit-department.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-department',
+  templateUrl: './add-edit-department.component.html',
+  styleUrl: './add-edit-department.component.css'
+})
+export class AddEditDepartmentComponent {
+
+}
